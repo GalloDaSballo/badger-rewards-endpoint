544afda89115f1493d53203b1dbfa790f9771e6d
Entreprenerd <valeranijr@gmail.com>
2022-07-13 00:05:33 +0200

    feat: initial setup

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.10;
 
 contract RewardsEndPoint {
   // All of the base code for any BadgerRewards Endpoint
   // Effectively a staking contract that holds funds and then sends rewards back to rewards manager allowing claims
 
-  
+
 }
---
+++ src/TheRewardsManager.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-3
+pragma solidity ^0.8.10;
+
+import {RewardsManager} from "lib/badger-onchain-rewards/contracts/RewardsManager.sol";
+
+contract TheRewardsManager is RewardsManager {}
